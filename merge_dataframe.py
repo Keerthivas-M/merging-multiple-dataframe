55cbfaa6f1d9d3c97ce092fd8c650896297682d3
Keerthivas-M <117818674+Keerthivas-M@users.noreply.github.com>
2022-11-09 15:32:47 +0530

    Add files via upload

---
+++ merge_dataframe.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+load = input("Enter the normal load")
+temp = input("Enter the temperature")
+lf1 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding1.profile', sep = ' ',skiprows=1)
+# lf1 = lf1.iloc[1:,:]
+# lf1.drop(index=lf1.index[0], axis=0, inplace=True)
+# print(lf1[:5])
+lf2 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding2.profile', sep = ' ',skiprows=1)
+lf2 = lf2.iloc[1:,:]
+lf3 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding3.profile', sep = ' ',skiprows=1)
+lf3 = lf3.iloc[1:,:]
+lf4 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding4.profile', sep = ' ',skiprows=1)
+lf4 = lf4.iloc[1:,:]
+lateral = [lf1, lf2, lf3, lf4]
+nan_value=0
+merged = pd.concat(lateral, join='outer', ignore_index=True).fillna(nan_value)
+merged.to_csv('merged_lf.csv')
+stdev_x = np.std(merged.v_virtualdispz[25:])
+avg_x = np.mean(merged.v_virtualdispz[25:])
+stdev_z = np.std(merged.v_pullvirtualy[25:])
+avg_z = np.mean(merged.v_pullvirtualy[25:])
+print(stdev_x, stdev_z, avg_x, avg_z)
