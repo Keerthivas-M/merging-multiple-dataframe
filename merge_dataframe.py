c3e102197d0f19299b59a5f56aaba8c686efe8e5
Keerthivas-M <117818674+Keerthivas-M@users.noreply.github.com>
2022-11-09 17:25:41 +0530

    Update merge_dataframe.py

--- merge_dataframe.py
+++ merge_dataframe.py
@@ -1,23 +1,26 @@
-#import pandas and numpy packages
 import pandas as pd
 import numpy as np
-#enter input values to id filename
+import matplotlib.pyplot as plt
 load = input("Enter the normal load")
 temp = input("Enter the temperature")
-#read multiple csv files space seperated and skip firstrow 
-lf1 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding1.profile', sep = ' ',skiprows=1)
-lf2 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding2.profile', sep = ' ',skiprows=1)
-lf2 = lf2.iloc[1:,:]
-lf3 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding3.profile', sep = ' ',skiprows=1)
-lf3 = lf3.iloc[1:,:]
-lf4 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding4.profile', sep = ' ',skiprows=1)
-lf4 = lf4.iloc[1:,:]
+lf1 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding1.profile', sep = ' ',skiprows=2, names=['TimeStep' ,'v_disx' ,'v_disy' ,'v_disz' ,'v_virtualdispx' ,'v_virtualdispy' ,'v_virtualdispz', 'v_pullvirtualx' ,'v_pullvirtualy', 'v_pullvirtualz' ,'c_contactforce[1]' ,'c_contactforce[2]' ,'c_contactforce[3]' ,'c_grforce[1]' ,'c_grforce[2]' ,'c_grforce[3]' ,'c_adhesion' ,'c_potential','c_intertemp)'])
+# lf1 = lf1.iloc[1:,:]
+# lf1.drop(index=lf1.index[0], axis=0, inplace=True)
+# print(lf1[:5])
+lf2 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding2.profile', sep = ' ',skiprows=2, names=['TimeStep' ,'v_disx' ,'v_disy' ,'v_disz' ,'v_virtualdispx' ,'v_virtualdispy' ,'v_virtualdispz', 'v_pullvirtualx' ,'v_pullvirtualy', 'v_pullvirtualz' ,'c_contactforce[1]' ,'c_contactforce[2]' ,'c_contactforce[3]' ,'c_grforce[1]' ,'c_grforce[2]' ,'c_grforce[3]' ,'c_adhesion' ,'c_potential','c_intertemp)'])
+lf3 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding3.profile', sep = ' ',skiprows=2, names=['TimeStep' ,'v_disx' ,'v_disy' ,'v_disz' ,'v_virtualdispx' ,'v_virtualdispy' ,'v_virtualdispz', 'v_pullvirtualx' ,'v_pullvirtualy', 'v_pullvirtualz' ,'c_contactforce[1]' ,'c_contactforce[2]' ,'c_contactforce[3]' ,'c_grforce[1]' ,'c_grforce[2]' ,'c_grforce[3]' ,'c_adhesion' ,'c_potential','c_intertemp)'])
+lf4 = pd.read_csv('p0d_'+load+'nN_'+temp+'K_sliding4.profile', sep = ' ',skiprows=2, names=['TimeStep' ,'v_disx' ,'v_disy' ,'v_disz' ,'v_virtualdispx' ,'v_virtualdispy' ,'v_virtualdispz', 'v_pullvirtualx' ,'v_pullvirtualy', 'v_pullvirtualz' ,'c_contactforce[1]' ,'c_contactforce[2]' ,'c_contactforce[3]' ,'c_grforce[1]' ,'c_grforce[2]' ,'c_grforce[3]' ,'c_adhesion' ,'c_potential','c_intertemp)'])
 lateral = [lf1, lf2, lf3, lf4]
 nan_value=0
 merged = pd.concat(lateral, join='outer', ignore_index=True).fillna(nan_value)
 merged.to_csv('merged_lf.csv')
-stdev_x = np.std(merged.v_virtualdispz[25:])
-avg_x = np.mean(merged.v_virtualdispz[25:])
-stdev_z = np.std(merged.v_pullvirtualy[25:])
-avg_z = np.mean(merged.v_pullvirtualy[25:])
+merged['sliding_dist'] = (merged['TimeStep'] - 100500) * 0.000004
+print(merged.head())
+stdev_x = np.std(merged.v_pullvirtualx[25:])
+avg_x = np.mean(merged.v_pullvirtualx[25:])
+stdev_z = np.std(merged.v_pullvirtualz[25:])
+avg_z = np.mean(merged.v_pullvirtualz[25:])
+#stderror.x = stdev_x/ sqrt()
 print(stdev_x, stdev_z, avg_x, avg_z)
+plt.plot(merged.sliding_dist[25:], merged.v_pullvirtualx[25:])
+plt.show()
